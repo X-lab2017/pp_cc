71c1ea09c4895b6d70ba50a4a76dd52c9276d2c4
515186469@qq.com <515186469@qq.com>
2019-05-19 12:47:03 +0800

    remove book validate

--- scripts/validate.js
+++ scripts/validate.js
@@ -19,22 +19,23 @@ if (document.location.host == 'gitcourse.kfcoding.com') {
         }
     });
 
-    var gitRepo = window.location.href.substr(window.location.href.indexOf("#")+1, window.location.href.length)
-
-    fetch('http://api.kfcoding.com/api/basic/scenes/isIn', {
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: token
-        },
-        method: 'POST',
-        body: JSON.stringify({repo: gitRepo})
-    }).then(resp => resp.json()).then(data => {
-        if (data.status !== 200) {
-            window.location.href = "http://kfcoding.com/user/login?redirect=" + window.location.href;
-        } else {
-            if(!data.data)
-                window.location.href = "http://kfcoding.com/account/authBook?redirect=" + window.location.href;
-        }
-    });
+    // 书籍明暗码认证
+    // var gitRepo = window.location.href.substr(window.location.href.indexOf("#")+1, window.location.href.length)
+    //
+    // fetch('http://api.kfcoding.com/api/basic/scenes/isIn', {
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         Authorization: token
+    //     },
+    //     method: 'POST',
+    //     body: JSON.stringify({repo: gitRepo})
+    // }).then(resp => resp.json()).then(data => {
+    //     if (data.status !== 200) {
+    //         window.location.href = "http://kfcoding.com/user/login?redirect=" + window.location.href;
+    //     } else {
+    //         if(!data.data)
+    //             window.location.href = "http://kfcoding.com/account/authBook?redirect=" + window.location.href;
+    //     }
+    // });
 
 }
